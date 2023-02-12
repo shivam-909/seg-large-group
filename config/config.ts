e2f40fcff5e62c0feb2a48189c06824510879016
shivam <86538330+shivam-909@users.noreply.github.com>
2023-02-12 23:38:22 +0000

    add auth routes + database connection

---
+++ config/config.ts
@@ -0,0 +1,9 @@
+// TODO(shivam): Add firebase service account key.
+export const FirebaseServiceAccount = {
+    apiKey: "AIzaSyB4cjZ5Qp6t1gBeTOk_W6IEyi33TbJz2aY",
+    authDomain: "seg-large-group-55a41.firebaseapp.com",
+    projectId: "seg-large-group-55a41",
+    storageBucket: "seg-large-group-55a41.appspot.com",
+    messagingSenderId: "550152717796",
+    appId: "1:550152717796:web:e7854baa0526b67d6a73f1"
+};
