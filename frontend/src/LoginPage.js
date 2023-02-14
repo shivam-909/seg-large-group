1beb7f5d954ce36c199d9c4013d1a32564409612
Oneeb Aamer <oneeb47@gmail.com>
2023-02-14 17:04:44 +0000

    Created basic login page

---
+++ frontend/src/LoginPage.js
@@ -0,0 +1,18 @@
+import './LoginPage.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Login Page
+        <div className='App-text'>
+        <input className='App-input' placeholder="Email"/>
+        <input className='App-input' placeholder="Password"/>
+        <button className='App-button'>Enter</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/LoginPage.test.js
+++ frontend/src/LoginPage.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import LgoinPage from './LoginPage';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<LoginPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
