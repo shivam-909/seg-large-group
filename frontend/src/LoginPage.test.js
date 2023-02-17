99b507e4d29efb22122655a677be41102c2319d9
Oneeb Aamer <oneeb47@gmail.com>
2023-02-17 17:34:52 +0000

    Add Login Page Test

--- frontend/src/LoginPage.test.js
+++ frontend/src/LoginPage.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import LgoinPage from './LoginPage';
 
-// test('renders learn react link', () => {
-//   render(<LoginPage />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<LoginPage />);
+  const linkElement = screen.getByText(/Login Page/i);
+  expect(linkElement).toBeInTheDocument();
+});
