ef3a60488cfbc1c3b37dae13800d815308e530e0
Oneeb Aamer <oneeb47@gmail.com>
2023-02-17 17:18:51 +0000

    Added fontawsome + password visibility toggle

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -1,18 +1,33 @@
 import './LoginPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function LoginPage() {
   function loginButton(){
     let email = document.getElementById("email").value
     let password = document.getElementById("password").value
   }
+  function hidePassword() {
+    let passwordField = document.getElementById("password");
+    let eye = document.getElementById("toggleEye");
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      eye.className = "fa-solid fa-eye-slash";
+    } else {
+      passwordField.type = "password";
+      eye.className = "fa-solid fa-eye"
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         Login Page
         <div className='App-text'>
-        <input id='email' className='App-input' placeholder="Email"/>
-        <input id='password' className='App-input' placeholder="Password"/>
-        <button className='button' onClick={loginButton}>Enter</button>
+        <input id='email' className='email-input' placeholder="Email"/>
+        <div>
+          <input id='password' type="password" className='password-input' placeholder="Password"/>
+          <button type='checkbox' className='togglePasswordButton' onClick={hidePassword}><i id='toggleEye' className="fa-solid fa-eye"></i></button>
+        </div>
+        <button className='button' onClick={loginButton}>Enter <i class="fa-solid fa-right-to-bracket"></i></button>
         </div>
       </header>
     </div>
