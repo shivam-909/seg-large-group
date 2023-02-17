e41e5eff75f56e4b89733e3ce3de2757c1e70331
Oneeb Aamer <oneeb47@gmail.com>
2023-02-17 17:20:23 +0000

    Fixed typo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LgoinPage from './LoginPage';
+import LoginPage from './LoginPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LgoinPage />
+    <LoginPage />
   </React.StrictMode>
 );
 
