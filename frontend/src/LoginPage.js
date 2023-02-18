53a0813113342c7d2eb0053be40ffd3f39d828f1
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-18 17:20:13 +0000

    Fix bugs

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -4,8 +4,18 @@ function LoginPage() {
   function loginButton(){
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    console.log(email);
-    console.log(password);
+    let rememberPass = document.getElementById("rememberPass").checked;
+    // console.log(email);
+    // console.log(password);
+    // Stores password and email locally if the user allowed it.
+    if (rememberPass === true){
+      localStorage.setItem("email", email)
+      localStorage.setItem("password", password)
+    }
+    else{
+      localStorage.removeItem("email", " ")
+      localStorage.removeItem("password", " ")
+    }
     // API backend needed to be added.
   }
   function hidePassword() {
@@ -25,17 +35,16 @@ function LoginPage() {
       <header className="LoginPage-header">
         Login Page
         <div className='LoginPage-text'>
-        <input id='email' className='email-input' placeholder="Email"/>
+        <input id='email' className='email-input' defaultValue={localStorage.getItem("email")} placeholder="Email"/>
         <div>
-          <input id='password' type="password" className='password-input' placeholder="Password"/>
+          <input id='password' type="password" className='password-input' defaultValue={localStorage.getItem("password")} placeholder="password"/>
           <button type='checkbox' className='togglePasswordButton' onClick={hidePassword}><i id='toggleEye' className="fa-solid fa-eye"></i></button>
         </div>
         <div>
-          <input class='w-5 h-5 inline-block' type='checkbox'/> 
-          <a> Remember Me</a>
+          <input id='rememberPass' class='w-5 h-5 inline-block' type='checkbox'/> Remember Me
         </div>
         <div class='mt-5'>
-        New user? <a className='LoginPage-link' href='/'>Sign up.</a>
+        New user? <a className='LoginPage-link' href='/#'>Sign up.</a>
         </div>
         <button className='button' onClick={loginButton}>Enter <i class="fa-solid fa-right-to-bracket"></i></button>
         </div>
