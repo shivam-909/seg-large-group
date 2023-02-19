d5c7140bb589e85e597619c61f41388fd80d884a
shivam <86538330+shivam-909@users.noreply.github.com>
2023-02-19 19:09:01 +0000

    fix db retrieval, make routes async

--- backend/service/public.ts
+++ backend/service/public.ts
@@ -1,14 +1,34 @@
-import { Application, Request, Response } from 'express';
+import { Application, NextFunction, Request, Response } from 'express';
 import DB from '../db/db';
 
 export interface Error {
     message: string;
 }
 
-export function getErrorMessage(error: unknown) {
-    if (error instanceof Error) return error.message
+export function getErrorMessage(error: any) {
+    if ('message' in error) return error.message
     return String(error)
 }
 
-export type Handler = (req: Request, res: Response) => void;
+export type Handler = (req: Request, res: Response, next: NextFunction) => void;
 export type HandlerWrapper = (db: DB) => Handler;
+
+export interface Token {
+    id: string,
+    type: string,
+    exp: number
+}
+
+export const ErrorInvalidEmail = "invalid email";
+export const ErrorInvalidPassword = "invalid password";
+export const ErrorUserExists = "user already exists";
+export const FailedToHashPassword = "failed to hash password";
+
+export var ErrorToCode: Map<string, number> = new Map<string, number>(
+    [
+        [ErrorInvalidEmail, 400],
+        [ErrorInvalidPassword, 400],
+        [ErrorUserExists, 400],
+        [FailedToHashPassword, 500],
+    ]
+);
