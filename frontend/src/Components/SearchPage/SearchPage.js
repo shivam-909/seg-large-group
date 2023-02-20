697df3450317866d1df2b39f86b06a96c537002f
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-20 16:00:32 +0000

    Add Search GUI

--- frontend/src/Components/SearchPage/SearchPage.js
+++ frontend/src/Components/SearchPage/SearchPage.js
@@ -1,9 +1,12 @@
+import SearchBar from "./SearchBar";
+
 function SearchPage() {
   return (
     <div className="LoginPage">
       <header className="LoginPage-header">
-        Search Page
-      </header>
+          Search Page
+          <SearchBar/>
+        </header>
     </div>
   );
 }
