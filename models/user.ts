ab71c4681d33b52c10f004d6538dafa29c9a9b56
Cem Ratip <cemratip@outlook.com>
2023-02-20 03:24:26 +0000

    partway through updating user model and crud functions

--- models/user.ts
+++ models/user.ts
@@ -1,14 +1,24 @@
 import { QueryDocumentSnapshot } from "firebase-admin/firestore";
 
 class User {
-    username: string;
-    hashedPassword: string;
+    first_name: string;
+    last_name: string;
+    date_of_birth: Date;
     email: string;
+    hashedPassword: string;
+    is_company: {
+        type: boolean,
+        default: false
+    };
+    company_name: string;
+    location: string;
+    pfp_url: string;
+    saved_jobs: string[];
+    notifications: string[];
 
-    constructor(username: string, hashedPassword: string, email: string) {
-        this.username = username;
-        this.hashedPassword = hashedPassword;
+    constructor(id: string, email: string, hashedPassword: string) {
         this.email = email;
+        this.hashedPassword = hashedPassword;
     }
 }
 
