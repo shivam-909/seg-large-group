9a002df2617ae590c4274a82f5704370ff85be2e
Samiul I <99480280+Samiul687@users.noreply.github.com>
2023-02-21 22:12:05 +0000

    Worked on seeder

--- backend/seeder/seed.ts
+++ backend/seeder/seed.ts
@@ -4,7 +4,7 @@ import JobListing from "../models/job";
 import { createJobListing } from "../db/jobs";
 
 class Seeder {
-  private db: DB;
+  private readonly db: DB;
 
   constructor(db: DB) {
     this.db = db;
@@ -13,23 +13,26 @@ class Seeder {
   // Define a function that generates a random job listing using the faker library
   private generateJobListing(): JobListing {
     return new JobListing(
-      faker.random.uuid(),
-      faker.name.jobTitle(),
-      faker.random.number({ min: 50000, max: 150000 }),
-      faker.lorem.paragraph(),
-      faker.address.city(),
-      faker.random.arrayElement(["Full-time", "Part-time", "Contract"]),
-      faker.company.companyName(),
-      faker.random.arrayElement(["Engineering", "Sales", "Marketing", "Finance"]),
-      faker.date.past(),
-      [faker.lorem.words(), faker.lorem.words(), faker.lorem.words()],
-      [faker.lorem.words(), faker.lorem.words(), faker.lorem.words()]
+        faker.datatype.uuid(),
+        faker.name.jobTitle(),
+        faker.datatype.number({
+          'min': 30000,
+          'max': 100000
+        }),
+        faker.lorem.paragraph(),
+        faker.address.city(),
+        faker.helpers.arrayElement(["Full-time", "Part-time", "Contract"]),
+        faker.company.name(),
+        faker.helpers.arrayElement(["Engineering", "Sales", "Marketing", "Finance"]),
+        faker.date.past(),
+        [faker.lorem.words(), faker.lorem.words(), faker.lorem.words()],
+        [faker.lorem.words(), faker.lorem.words(), faker.lorem.words()]
     );
   }
 
   // Define a function that creates a specified number of fake job listings and adds them to the database
-  async seedJobListings(count: number): Promise<void> {
-    for (let i = 0; i < count; i++) {
+  async seedJobListings(): Promise<void> {
+    for (let i = 0; i < 20; i++) {
       const jobListing = this.generateJobListing();
       await createJobListing(this.db, jobListing);
     }
