a8b5d114d92e5b660146e4a6aaf4282813d1f571
Samiul I <99480280+Samiul687@users.noreply.github.com>
2023-02-21 20:53:04 +0000

    Update db.ts

--- backend/db/db.ts
+++ backend/db/db.ts
@@ -1,6 +1,7 @@
 import { CollectionReference, DocumentData, Firestore } from 'firebase-admin/firestore';
 import { FirebaseServiceAccount } from '../config/config';
 import { UserConverter } from '../models/user';
+import { JobListingConverter } from '../models/job';
 
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
@@ -22,6 +23,10 @@ class DB {
 
     UserCollection() {
         return this.db.collection('users').withConverter(UserConverter);
+    }
+
+    JobListingCollection() {
+        return this.db.collection('jobs').withConverter(JobListingConverter);
     }
 }
 
