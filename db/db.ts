2ab758b235d0cde6bf1aed5fccce717bc0d150e1
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2023-02-21 17:52:43 +0000

    application model created and CRUD functions made

--- db/db.ts
+++ db/db.ts
@@ -1,12 +1,14 @@
 import { CollectionReference, DocumentData, Firestore } from 'firebase-admin/firestore';
 import { FirebaseServiceAccount } from '../config/config';
 import { UserConverter } from '../models/user';
+import {ApplicationConverter} from "../models/application";
 
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
 interface DB {
     UserCollection(): CollectionReference<DocumentData>;
+    ApplicationCollection(): CollectionReference<DocumentData>;
 }
 
 class DB {
@@ -23,6 +25,11 @@ class DB {
     UserCollection() {
         return this.db.collection('users').withConverter(UserConverter);
     }
+
+    ApplicationCollection() {
+        return this.db.collection('applications').withConverter(ApplicationConverter);
+    }
+
 }
 
 export default DB;
---
+++ models/enums/status.enum.ts
@@ -0,0 +1,6 @@
+export enum Status {
+    Applied,
+    Interview,
+    Rejected,
+    Archived,
+}
