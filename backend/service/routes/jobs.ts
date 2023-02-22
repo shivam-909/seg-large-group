0dabbfb9baad4071d662edffe62a05a059787607
Samiul I <99480280+Samiul687@users.noreply.github.com>
2023-02-22 08:03:25 +0000

    Added route for retrieving job listings

--- backend/service/routes/jobs.ts
+++ backend/service/routes/jobs.ts
@@ -1,16 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import DB from "../../db/db";
 import JobListing from "../../models/job";
-import { createJobListing } from "../../db/jobs";
+import { createJobListing, retrieveJobListing} from "../../db/jobs";
 import { Error, getErrorMessage, Handler } from "../public";
 import {seedJobListings} from "../../seeder/seed";
 import { randomUUID } from "crypto";
 
-// AddJobListing accepts a request containing the job listing data, and creates a new job listing in the database.
 export function AddListing(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
         const { title, compensation, description, location, schedule, companyName, type, datePosted, benefits, requirements } = req.body;
-        const newID = randomUUID(); // generate ID based on timestamp
+        const newID = randomUUID();
         const newJobListing = new JobListing(newID, title, compensation, description, location, schedule, companyName, type, datePosted, benefits, requirements);
 
         try {
@@ -26,11 +25,37 @@ export function AddListing(db: DB): Handler {
     }
 }
 
+export function GetListing(db: DB): Handler {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params;
+
+        try {
+            const jobListing = await retrieveJobListing(db, id);
+            if (jobListing) {
+                res.status(200).json(jobListing);
+            } else {
+                res.status(404).json({
+                    message: 'Job listing not found'
+                });
+            }
+        } catch (err) {
+            next({
+                message: getErrorMessage(err),
+            });
+        }
+    };
+}
+
+
+
+
 export function SeedJobs(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
             await seedJobListings(db);
-            res.status(200).json({ message: 'Job listings seeded successfully' });
+            res.status(200).json({
+                message: 'Job listings seeded successfully'
+            });
         } catch (err) {
             next(err);
         }
