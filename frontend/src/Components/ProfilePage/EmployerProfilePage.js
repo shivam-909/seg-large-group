bda59089c57fcb7800f61b4d947addb8c95a78ea
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-02-23 11:47:06 +0000

    Completed main parts of profile pages

--- frontend/src/Components/ProfilePage/EmployerProfilePage.js
+++ frontend/src/Components/ProfilePage/EmployerProfilePage.js
@@ -1,4 +1,5 @@
 import './ProfilePage.css';
+//import TextInputBox from './Components/LoginPage/TextInputBox.js';
 import React, {useState} from 'react';
 
 function EmployerProfilePage() {
@@ -26,37 +27,33 @@ function EmployerProfilePage() {
     }
 
   return (
-    <div className="ProfilePage">
-      <header className="ProfilePage-header">
-        Your Profile:
-        {!isEditing && <button className="edit-btn" onClick={EditOnClick} >Edit Profile</button>}
+  <div className='bg-dark-theme-grey min-h-screen items-center justify-center flex'>
+    <div className='bg-lighter-grey rounded-md sm:min-w-1/6 inline-grid px-12 py-7 space-y-3'>
+      <header className='mb-6 font-bold text-2xl flex justify-center'>Your Profile Page</header>
+        {!isEditing && <button className='edit-btn' onClick={EditOnClick} >Edit Profile</button>}
         <div className='ProfilePage-text' id="profile">
           {!isEditing && (
             <>
-            <p>Company Name: {!isEditing && <span id="companyName">{profile.companyName}</span>}</p>
-            <p>Email: {!isEditing && <span id="firstName">{profile.email}</span>}</p>
-            <p>Phone Number: {!isEditing && <span id="phoneNum">{profile.phoneNum}</span>}</p>
-            <p>Address: {!isEditing && <span id="address">{profile.address}</span>}</p>
-            <p>Company Description: {!isEditing && <span id="companyDescrip">{profile.companyDescrip}</span>}</p>
-            </>
-        )}
+            <p><strong>Company Name:</strong> <br />{!isEditing && <span id="companyName">{profile.companyName}</span>}</p>
+            <p><strong>Email:</strong> <br />{!isEditing && <span id="firstName">{profile.email}</span>}</p>
+            <p><strong>Phone Number:</strong>  <br />{!isEditing && <span id="phoneNum">{profile.phoneNum}</span>}</p>
+            <p><strong>Address:</strong> <br />{!isEditing && <span id="address">{profile.address}</span>}</p>
+            <p><strong>Company Description:</strong> <br />{!isEditing && <span id="companyDescrip">{profile.companyDescrip}</span>}</p>
+            </>  )}
         </div>
         {isEditing && <button className="save-btn" onClick={SaveOnClick} >Save Profile</button>}
-        <div id="profile">
+        <div className='text-input' id="profile">
         {isEditing && (
           <>
-           <p>Company Name: <input type="text" id="companyName" value= {profile.companyName} onChange={EditProfile}/></p>
-           <p>Email: <input type="text" id="email" value= {profile.email} onChange={EditProfile}/></p>
-           <p>Phone Number: <input type="text" id="skills" value= {profile.phoneNum} onChange={EditProfile}/></p>
-           <p>Address: <input type="text" id="education" value= {profile.address} onChange={EditProfile}/></p>
-           <p>Company Description: <input type="text" id="previousEmployment" value= {profile.companyDescrip} onChange={EditProfile}/></p>
-
-          </>
-        )}
-
+           <p><strong>Company Name:</strong> <input type="text" id="companyName" value= {profile.companyName} onChange={EditProfile}/></p>
+           <p><strong>Email: </strong> <input type="text" id="email" value= {profile.email} onChange={EditProfile}/></p>
+           <p><strong>Phone Number: </strong> <input type="text" id="phoneNum" value= {profile.phoneNum} onChange={EditProfile}/></p>
+           <p><strong>Address: </strong> <input type="text" id="address" value= {profile.address} onChange={EditProfile}/></p>
+           <p><strong>Company Description:</strong>  <input type="text" id="companyDescrip" value= {profile.companyDescrip} onChange={EditProfile}/></p>
+          </>)}
         </div>
-        </header>
-    </div>
+   </div>
+ </div>
   );
 }
 
--- frontend/src/Components/ProfilePage/UserProfilePage.js
+++ frontend/src/Components/ProfilePage/UserProfilePage.js
@@ -27,38 +27,35 @@ function UserProfilePage() {
     }
 
   return (
-    <div className="ProfilePage">
-      <header className="ProfilePage-header">
+    <div className='bg-dark-theme-grey min-h-screen items-center justify-center flex'>
+      <div className='bg-lighter-grey rounded-md sm:min-w-1/6 inline-grid px-12 py-7 space-y-3'>
+        <header className='mb-6 font-bold text-2xl flex justify-center'>Your Profile Page</header>
         Your Profile:
         {!isEditing && <button className="edit-btn" onClick={EditOnClick} >Edit Profile</button>}
         <div className='ProfilePage-text' id="profile">
           {!isEditing && (
             <>
-            <p>First Name: {!isEditing && <span id="firstName">{profile.firstName}</span>}</p>
-            <p>Last Name: {!isEditing && <span id="lastName">{profile.lastName}</span>}</p>
-            <p>Email: {!isEditing && <span id="email">{profile.email}</span>}</p>
-            <p>Skills: {!isEditing && <span id="skills">{profile.skills}</span>}</p>
-            <p>Education: {!isEditing && <span id="education">{profile.education}</span>}</p>
-            <p>Previous Employment: {!isEditing && <span id="previousEmployment">{profile.previousEmployment}</span>}</p>
-            </>
-        )}
+            <p><strong> First Name: </strong> <br />{!isEditing && <span id="firstName">{profile.firstName}</span>}</p>
+            <p><strong>Last Name: </strong> <br />{!isEditing && <span id="lastName">{profile.lastName}</span>}</p>
+            <p><strong>Email:</strong>  <br />{!isEditing && <span id="email">{profile.email}</span>}</p>
+            <p><strong>Skills: </strong> <br />{!isEditing && <span id="skills">{profile.skills}</span>}</p>
+            <p><strong>Education: </strong> <br />{!isEditing && <span id="education">{profile.education}</span>}</p>
+            <p><strong>Previous Employment: </strong> <br />{!isEditing && <span id="previousEmployment">{profile.previousEmployment}</span>}</p>
+            </>)}
         </div>
         {isEditing && <button className="save-btn" onClick={SaveOnClick} >Save Profile</button>}
-        <div className='ProfilePage-text' id="profile">
+        <div className='text-input' id="profile">
         {isEditing && (
           <>
-           <p>First Name: <input type="text" id="firstName" value= {profile.firstName} onChange={EditProfile}/></p>
-           <p>Last Name: <input type="text" id="lastName" value= {profile.lastName} onChange={EditProfile}/></p>
-           <p>Email: <input type="text" id="email" value= {profile.email} onChange={EditProfile}/></p>
-           <p>Skills: <input type="text" id="skills" value= {profile.skills} onChange={EditProfile}/></p>
-           <p>Education: <input type="text" id="education" value= {profile.education} onChange={EditProfile}/></p>
-           <p>Previous Employment: <input type="text" id="previousEmployment" value= {profile.previousEmployment} onChange={EditProfile}/></p>
-
-          </>
-        )}
-
+           <p><strong>First Name: </strong> <input type="text" id="firstName" value= {profile.firstName} onChange={EditProfile}/></p>
+           <p><strong>Last Name: </strong> <input type="text" id="lastName" value= {profile.lastName} onChange={EditProfile}/></p>
+           <p><strong>Email:</strong>  <input type="text" id="email" value= {profile.email} onChange={EditProfile}/></p>
+           <p><strong>Skills:</strong>  <input type="text" id="skills" value= {profile.skills} onChange={EditProfile}/></p>
+           <p><strong>Education: </strong> <input type="text" id="education" value= {profile.education} onChange={EditProfile}/></p>
+           <p><strong>Previous Employment: </strong> <input type="text" id="previousEmployment" value= {profile.previousEmployment} onChange={EditProfile}/></p>
+          </> )}
         </div>
-      </header>
+      </div>
     </div>
   );
 }
