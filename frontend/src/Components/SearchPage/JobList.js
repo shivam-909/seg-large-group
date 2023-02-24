21c4e5e6c25243a1a9495ca2685299cee237360f
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-24 22:57:58 +0000

    Implement functionality behind infinite scroll

---
+++ frontend/src/Components/SearchPage/JobList.js
@@ -0,0 +1,74 @@
+import {useEffect} from "react";
+function JobList() {
+    // const countElem = document.getElementById("count");
+    // const totalElem = document.getElementById("total");
+    const limit = 99;
+    const increase = 9;
+    const pageCount = Math.ceil(limit / increase);
+    let currentPage = 1;
+    async function createCard(index){
+        const card = await document.createElement("div");
+        const container = await document.getElementById("container");
+        card.className = "p-6 m-2 border-2 border-black";
+        card.innerHTML = index;
+        container.appendChild(card);
+    };
+    async function addCards(pageIndex){
+        currentPage = pageIndex;
+        const startRange = (pageIndex - 1) * increase;
+        const endRange = currentPage == pageCount ? limit : pageIndex * increase;
+        // countElem.innerHTML = " " + endRange;
+
+        for (let i = startRange + 1; i <= endRange; i++) {
+            await createCard(i);
+        }
+    };
+    var throttleTimer;
+    async function throttle(callback, time){
+        if (throttleTimer) return;
+        throttleTimer = true;
+        setTimeout(() => {
+            callback();
+            throttleTimer = false;
+        }, time);
+    };
+    const removeInfiniteScroll = () => {
+        window.removeEventListener("scroll", handleInfiniteScroll);
+    };
+    async function handleInfiniteScroll(){
+        throttle(async () => {
+            console.log(currentPage)
+            const endOfPage =
+                window.innerHeight + window.scrollY >= document.body.offsetHeight - 2;
+            if (endOfPage) {
+                await addCards(currentPage + 1);
+            }
+            if (currentPage === pageCount) {
+                console.log("STOPPED")
+                removeInfiniteScroll();
+            }
+        }, 500);
+    };
+    // if (document.readyState === 'complete') {
+    //     console.log("ready")
+    //     window.addEventListener("scroll", handleInfiniteScroll);
+    //     addCards(currentPage);
+    // }
+    window.onload = function () {
+        window.addEventListener("scroll", handleInfiniteScroll);
+        addCards(currentPage);
+    }
+    return (
+        <div>
+            <div id="container"></div>
+            <div className='action'>
+                <span>Showing
+                    <span id="count"> </span> of
+                    <span id="total"> {limit}</span> jobs
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default JobList;
--- frontend/src/Components/SearchPage/SearchPage.js
+++ frontend/src/Components/SearchPage/SearchPage.js
@@ -1,13 +1,16 @@
 import Navbar from "../Navbar/Navbar";
 import SearchBar from "./SearchBar";
-
+import JobList from "./JobList";
 function SearchPage() {
   return (
       <div>
           <Navbar/>
           <div className="bg-lighter-grey min-h-screen items-center justify-center flex">
-              <header className="bg-white rounded-md sm:min-w-1/6 inline-grid px-12 py-7 space-y-3">
+              <header className="mt-24 bg-white rounded-md sm:min-w-1/6 inline-grid px-12 py-7 space-y-3">
                   <SearchBar/>
+                  <div className="">
+                      <JobList/>
+                  </div>
               </header>
           </div>
       </div>
