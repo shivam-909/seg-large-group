b6827c4814601428b90e7279e1a34e54d47ad777
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-24 13:32:59 +0000

    Change Search Page to Light theme

--- frontend/src/Components/SearchPage/SearchPage.js
+++ frontend/src/Components/SearchPage/SearchPage.js
@@ -3,13 +3,14 @@ import SearchBar from "./SearchBar";
 
 function SearchPage() {
   return (
-    <div className="LoginPage">
-      <Navbar/>
-      <header className="LoginPage-header">
-          Search Page
-          <SearchBar/>
-      </header>
-    </div>
+      <div>
+          <Navbar/>
+          <div className="bg-lighter-grey min-h-screen items-center justify-center flex">
+              <header className="bg-white rounded-md sm:min-w-1/6 inline-grid px-12 py-7 space-y-3">
+                  <SearchBar/>
+              </header>
+          </div>
+      </div>
   );
 }
 
