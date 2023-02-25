ef8407f6a475d0450599f2ad2f65a8cbebce0aa6
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-25 01:11:03 +0000

    Change scroll numbers to Job Cards

--- frontend/src/Components/SearchPage/JobList.js
+++ frontend/src/Components/SearchPage/JobList.js
@@ -1,4 +1,5 @@
 import {useEffect} from "react";
+import JobPostCard from "./JobPostCard";
 function JobList() {
     // const countElem = document.getElementById("count");
     // const totalElem = document.getElementById("total");
@@ -7,10 +8,30 @@ function JobList() {
     const pageCount = Math.ceil(limit / increase);
     let currentPage = 1;
     async function createCard(index){
+        // <JobPostCard
+        // title='Web Developer' age={2} location='London' types={['Full-time', 'Part-time']}
+        // companyName='Expedia' salary='30,000/year' urgent={true} requirements={['C#: 2 years']}
+        // benefits={['Free transport']}
+        // />
+        // card.className = "p-6 m-2 border-2 border-black";
+        // card.innerHTML = index;
         const card = await document.createElement("div");
+        card.innerHTML = "<JobPostCard title='Web Developer' age={2} location='London' types={['Full-time', 'Part-time']} companyName='Expedia' salary='30,000/year' urgent={true} requirements={['C#: 2 years']} benefits={['Free transport']} />"
         const container = await document.getElementById("container");
-        card.className = "p-6 m-2 border-2 border-black";
-        card.innerHTML = index;
+        // card.setAttribute("title", 'Web Developer');
+        // card.age = 2;
+        // card.setAttribute("location", 'London');
+        // card.setAttribute('types' , "['Full-time', 'Part-time']");
+        // card.setAttribute("companyName", "Expedia")
+        // card.setAttribute("salary", "30,000/year")
+        // card.setAttribute("urgent", "true")
+        // card.setAttribute("requirements", "['C#: 2 years']")
+        // card.setAttribute("benefits", '["Free transport"]')
+        // card.companyName = "Expedia";
+        // card.salary = '30,000/year'
+        // card.urgent = true;
+        // card.requirements = ['C#: 2 years']
+        // card.benefits = ['Free transport']
         container.appendChild(card);
     };
     async function addCards(pageIndex){
