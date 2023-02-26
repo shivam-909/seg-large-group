f4ce899a65312e4581ea8c3b1ee626f49ccd5cdf
Cem Ratip <cemratip@outlook.com>
2023-02-26 19:57:03 +0000

    fix file storage bug

--- service/storage/storage.ts
+++ service/storage/storage.ts
@@ -1,30 +1,38 @@
 import * as AWS from 'aws-sdk';
-import { S3 } from 'aws-sdk';
 import { Error, ErrorUserExists, getErrorMessage, Handler } from "../public";
 import {Request, Response} from "express";
 require('dotenv').config();
 
 const bucket: string = `seg-joblink`;
-const s3: S3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const S3 = new AWS.S3({
+    accessKeyId: 'AKIAWQAQTTQM4KT6WI7Y',
+    secretAccessKey: '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',
+    region: 'eu-west-2',
 });
 export function uploadFile() : Handler {
-    console.log("test")
     return (req: Request, res: Response) => {
-        const { file } = req.body;
-        const splitFileNameByDot = file.originalname.split('.');
-        const fileExtension = splitFileNameByDot[splitFileNameByDot.length - 1];
-        let uploadedFile = s3
-            .upload({
+        const file = req.file;
+        if (file == null) {
+            return;
+        }
+        S3.upload({
                 Bucket: bucket,
-                Key: `dev/${file.originalname}.${fileExtension}`,
+                Key: `${file.originalname}`,
                 Body: file.buffer,
                 ContentType: file.mimetype,
-            })
-        return res.status(200).json({
-            URL: uploadedFile,
-        });
+            }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({
+                        message: err,
+                    });
+                }
+                console.log(data.Location);
+            });
+
+        // return res.status(200).json({
+        //     URL: uploadedFile,
+        // });
     }
 
 // async function deleteFile(key: any) {
