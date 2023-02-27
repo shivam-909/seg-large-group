b073bbab370dbd71c600a28c8a061526846c0abb
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-27 00:38:37 +0000

    Registration Page Refactored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './Components/LoginPage/LoginPage';
 import SearchPage from './Components/SearchPage/SearchPage';
+import RegisterPage from "./Components/RegisterPage/RegisterPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <LoginPage/> }/>
+        <Route path="/" element={ <RegisterPage/> }/>
         <Route path="/search" element={ <SearchPage/> }/>
       </Routes>
     </BrowserRouter>
