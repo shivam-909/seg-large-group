6c22b21f0ad3d5e4935eb4f9db3b08956644f6d4
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-27 18:57:26 +0000

    Change Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './Components/LoginPage/LoginPage';
 import SearchPage from './Components/SearchPage/SearchPage';
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <LoginPage/> }/>
-        <Route path="/search" element={ <SearchPage/> }/>
+          <Route path="/search" element={ <LoginPage/> }/>
+          <Route path="/" element={ <SearchPage/> }/>
       </Routes>
     </BrowserRouter>
   );
