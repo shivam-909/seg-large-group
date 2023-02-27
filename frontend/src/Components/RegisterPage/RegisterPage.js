28de16586a62964df69109a8fb1698f89a07f3e5
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-02-27 18:27:18 +0000

    Refactor Validation Fields

--- frontend/src/Components/RegisterPage/RegisterPage.js
+++ frontend/src/Components/RegisterPage/RegisterPage.js
@@ -8,18 +8,30 @@ import {useState} from "react";
 
 function RegisterPage() {
   const [errorMsg, setErrorMsg] = useState("")
+  function checkPasswordMatch(){
+    let pass = document.getElementById("password").value;
+    let confirmPass = document.getElementById("confirmPass").value;
+    if (confirmPass == pass){
+      setVisible("confirmPassError", false);
+    }
+    else{
+      setVisible("confirmPassError", true);
+      let errorText = document.getElementById("confirmPassError");
+      errorText.className = "block text-red left-2 relative";
+    }
+  }
   function validateField(field, regex, msg){
     let obj = document.getElementById(field);
     if (regex.test(obj.value)){
-      setErrorMsg("")
-      setVisible("errorBox", false);
+      setVisible(field+"Error", false);
     }
     else{
-      setErrorMsg(msg)
-      setVisible("errorBox", true);
+      setVisible(field+"Error", true);
+      let errorText = document.getElementById(field + "Error");
+      errorText.className = "block text-red left-2 relative";
     }
   }
-  function loginButton() {
+  function signUpButton() {
     // let email = document.getElementById("email").value;
     // let password = document.getElementById("password").value;
     // let rememberLogin = document.getElementById("rememberLogin").checked;
@@ -64,19 +76,35 @@ function RegisterPage() {
             <p className='mb-6 font-bold text-2xl flex justify-center'>Register an account</p>
 
             <BinaryOption option1={"Job Seeker"} function1={() => {toggleRole("Seeker-Fields")}} function2={() => {toggleRole("Company-Fields")}} option2={"Company"}/>
-            <div id="Seeker-Fields" className="space-x-2">
-              <TextInputBox id='FirstName' onBlur={()=>{validateField("FirstName",/^[A-Za-z]+$/,"Invalid First Name")}} placeholder='First Name'/>
-              <TextInputBox id='LastName' onBlur={()=>{validateField("LastName",/^[A-Za-z]+$/,"Invalid Last Name")}} placeholder='Last Name'/>
+            <div id="Seeker-Fields" className="space-x-2 inline">
+              <div className={"inline float-left"}>
+              <TextInputBox id='FirstName' onChange={()=>{validateField("FirstName",/^[A-Za-z]+$/,"Invalid First Name")}} placeholder='First Name'/>
+                <span id="FirstNameError" className={"invisible absolute top-0"}>Invalid First Name</span>
+              </div>
+              <div className={"inline float-right"}>
+                <TextInputBox id='LastName' onChange={()=>{validateField("LastName",/^[A-Za-z]+$/,"Invalid Last Name")}} placeholder='Last Name'/>
+                <span id="LastNameError" className={"invisible absolute top-0"}>Invalid Last Name</span>
+              </div>
             </div>
-            <div id="Company-Fields" className="invisible absolute top-0">
-              <TextInputBox id='CompanyName' placeholder='Company Name'/>
+            <div id="Company-Fields" className="invisible absolute top-0 w-full">
+              <TextInputBox id='CompanyName' className={"w-full"} placeholder='Company Name'/>
             </div>
 
-            {/*eslint-disable-next-line*/}
-            <TextInputBox id='email' onBlur={()=>{validateField("email",/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Invalid Email")}} placeholder='Email address'/>
+            <div className={"w-full"}>
+              {/*eslint-disable-next-line*/}
+            <TextInputBox id='email' className="w-full" onChange={()=>{validateField("email",/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Invalid Email")}} placeholder='Email address'/>
+            <span id="emailError" className={"invisible absolute top-0"}>Invalid Email</span>
+            </div>
 
-            <TextInputBoxWithIcon id='password' type='password' onBlur={()=>{validateField("password",/^(?=.*\s)/,"Invalid Password")}} placeholder='Password' icon={<img id='togglePassword' src={showIcon} alt='' onClick={() => {togglePasswordVisibility("password","togglePassword")}} className='cursor-pointer'/>}/>
-            <TextInputBox id='confirmPass' type='password' placeholder='Confirm Password'/>
+            <div>
+            <TextInputBoxWithIcon id='password' type='password' onChange={()=>{validateField("password",/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})/,"Invalid Password"); checkPasswordMatch()}} placeholder='Password' icon={<img id='togglePassword' src={showIcon} alt='' onClick={() => {togglePasswordVisibility("password","togglePassword")}} className='cursor-pointer'/>}/>
+            <span id="passwordError" className={"invisible absolute top-0"}>Invalid Password</span>
+            </div>
+
+            <div>
+            <TextInputBox id='confirmPass' className="w-full" onChange={checkPasswordMatch} type='password' placeholder='Confirm Password'/>
+            <span id="confirmPassError" className={"invisible absolute top-0"}>Password does not match</span>
+            </div>
 
             <div id="errorBox" className="invisible absolute top-96">
               <div className="border-2 border-red bg-light-red rounded-md p-2 flex items-center justify-center">
@@ -85,7 +113,7 @@ function RegisterPage() {
             </div>
 
             <div className='p-0.5'></div>
-            <button className='bg-dark-theme-grey rounded-md text-white p-2.5 flex items-center justify-center space-x-2' onClick={loginButton}>
+            <button className='bg-dark-theme-grey rounded-md text-white p-2.5 flex items-center justify-center space-x-2' onClick={signUpButton}>
               <p>Sign Up</p>
               <i className="fa-solid fa-right-to-bracket"></i>
             </button>
