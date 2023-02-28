1db4419cd735a8d49dfbefac3d4d80784fed2786
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-02-28 20:04:46 +0000

    button for notification is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 //import Welcome from './Components/LoginPage/Welcome';
 // import LoginPage from './Components/LoginPage/LoginPage';
-import EmployerProfilePage from './Components/ProfilePage/EmployerProfilePage';
+import EmployerNotificationPage from './Components/NotificationPage/EmployerNotificationPage';
 import SearchPage from './Components/SearchPage/SearchPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <EmployerProfilePage/> }/>
+        <Route path="/" element={ <EmployerNotificationPage/> }/>
         <Route path="/search" element={ <SearchPage/> }/>
       </Routes>
     </BrowserRouter>
