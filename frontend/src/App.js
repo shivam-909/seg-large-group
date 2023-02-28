4c15b64d46c38a840b82224024007886e66e8f20
Tahseen Humaira <humairatahseen4@gmail.com>
2023-02-28 11:37:21 +0000

    Update register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './Components/LoginPage/LoginPage';
 import SearchPage from './Components/SearchPage/SearchPage';
-<<<<<<< HEAD
-import RegistrationPage from './Components/RegistrationPage/Registration';
-=======
 import RegisterPage from "./Components/RegisterPage/RegisterPage";
->>>>>>> 656d78f499637d8ec3791e9452ee72068f8bc9e2
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={ <RegistrationPage/> }/>
-        <Route path="/login" element={ <LoginPage/> }/>
-        <Route path="/search" element={ <SearchPage/> }/>
-=======
           <Route path="/" element={ <SearchPage/> }/>
           <Route path="/login" element={ <LoginPage/> }/>
           <Route path="/signup" element={ <RegisterPage/> }/>
->>>>>>> 656d78f499637d8ec3791e9452ee72068f8bc9e2
       </Routes>
     </BrowserRouter>
   );
