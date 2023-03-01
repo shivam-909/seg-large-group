84e418cbd32f86ef63a0c26392226b0b85129c7d
Aamer, Oneeb (k21003974) <oneeb.aamer@kcl.ac.uk>
2023-03-01 18:06:36 +0000

    Add MyJobs page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './Components/LoginPage/LoginPage';
 import SearchPage from './Components/SearchPage/SearchPage';
 import RegisterPage from "./Components/RegisterPage/RegisterPage";
-
+import MyJobs from "./Components/MyJobs/MyJobs";
 export default function App() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ export default function App() {
           <Route path="/" element={ <SearchPage/> }/>
           <Route path="/login" element={ <LoginPage/> }/>
           <Route path="/signup" element={ <RegisterPage/> }/>
+          <Route path="/saved" element={ <MyJobs/> }/>
       </Routes>
     </BrowserRouter>
   );
