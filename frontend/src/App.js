e6939b5857422a435f4c1cfada458da29ebf124d
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-03-03 09:45:12 +0000

    Completed profile page for employer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import UserProfilePage from './Components/ProfilePage/UserProfilePage';
+import EmployerProfilePage from './Components/ProfilePage/EmployerProfilePage';
 import SearchPage from './Components/SearchPage/SearchPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <UserProfilePage/> }/>
+        <Route path="/" element={ <EmployerProfilePage/> }/>
         <Route path="/search" element={ <SearchPage/> }/>
       </Routes>
     </BrowserRouter>
