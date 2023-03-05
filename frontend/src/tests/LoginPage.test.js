cfa0a4d06c02e3366d949eaf274631d74ab7ff05
Cem Ratip <cemratip@outlook.com>
2023-03-05 19:16:44 +0000

    ignore axios import config

--- frontend/src/tests/LoginPage.test.js
+++ frontend/src/tests/LoginPage.test.js
@@ -3,7 +3,7 @@ import LoginPage from '../Components/LoginPage/LoginPage';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 test('renders Login Page', () => {
-  // render(<BrowserRouter><Routes><Route element={ <LoginPage/> }/></Routes></BrowserRouter>);
+  render(<BrowserRouter><Routes><Route element={ <LoginPage/> }/></Routes></BrowserRouter>);
   // const signInTitle = screen.getAllByText("Sign in to your account");
   // expect(signInTitle).toBeInTheDocument();
 });
