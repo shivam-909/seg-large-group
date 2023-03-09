f1a21083c4a0fdddd988a5de675cd0d8cfe19543
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-03-09 21:13:07 +0000

    Completed Register Page tests

---
+++ frontend/src/tests/RegisterPage.test.js
@@ -0,0 +1,84 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import RegisterPage from '../Components/RegisterPage/RegisterPage';
+import userEvent from "@testing-library/user-event";
+
+describe("RegisterPage", () => {
+  test("renders RegisterPage component", async () => {
+    render(<RegisterPage />);
+    const header = await screen.findByText(/register an account/i);
+    expect(header).toBeInTheDocument();
+  });
+  test("shows password when clicked on show icon", async () => {
+    render(<RegisterPage />);
+    const toggleEye = await screen.findByAltText("toggle password visibilty");
+    userEvent.click(toggleEye);
+    const passwordInput = await screen.findByLabelText(/password/i);
+    expect(passwordInput).toHaveAttribute("type", "text");
+  });
+
+  test("hides password when clicked on show icon", async () => {
+    render(<RegisterPage />);
+    const toggleEye = await screen.findByAltText("toggle password visibilty");
+    userEvent.click(toggleEye);
+    userEvent.click(toggleEye);
+    const passwordInput = await screen.findByLabelText(/password/i);
+    expect(passwordInput).toHaveAttribute("type", "password");
+  });
+
+  test("password validation error message is visible when entering a invalid password", async () => {
+    render(<RegisterPage />);
+    const passwordInput = await screen.findByLabelText(/password/i);
+    fireEvent.blur(passwordInput, { target: { value: "password"} });
+    const passwordError = await screen.findByText(/invalid password/i);
+    expect(passwordError).toBeInTheDocument();
+  });
+
+  test("password validation error message is not visible when entering a valid password", async () => {
+    render(<RegisterPage />);
+    const passwordInput = await screen.findByLabelText(/password/i);
+    fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
+    await waitFor(() => {
+      const passwordError = screen.queryByText(/invalid password/i);
+      expect(passwordError).not.toBeInTheDocument();
+    });
+  });
+
+  test("email validation error message is visible when entering a invalid email", async () => {
+    render(<RegisterPage />);
+    const emailInput = await screen.findByLabelText(/email address/i);
+    fireEvent.blur(emailInput, { target: { value: "notanemail"} });
+    const emailError = await screen.findByText(/invalid email/i);
+    expect(emailError).toBeInTheDocument();
+  });
+
+  test("email validation error message is not visible when entering a valid email", async () => {
+    render(<RegisterPage />);
+    const emailInput = await screen.findByLabelText(/email address/i);
+    fireEvent.blur(emailInput, { target: { value: "test@example.com"} });
+    await waitFor(() => {
+      const emailError = screen.queryByText(/invalid email/i);
+      expect(emailError).not.toBeInTheDocument();
+    });
+  });
+
+  test("password mismatch error message is visible when entering two different passwords", async () => {
+    render(<RegisterPage />);
+    const passwordInput = await screen.findByLabelText(/password/i);
+    const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
+    fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
+    fireEvent.blur(confirmPasswordInput, { target: { value: "DifferentPassword123!"} });
+    const passwordMismatchError = await screen.findByText(/passwords do not match/i);
+    expect(passwordMismatchError).toBeInTheDocument();
+  });
+
+  test("password mismatch error message is not visible when entering two same passwords", async () => {
+    render(<RegisterPage />);
+    const passwordInput = await screen.findByLabelText(/password/i);
+    const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
+    fireEvent.blur(passwordInput, { target: { value: "TestPassword123!"} });
+    fireEvent.blur(confirmPasswordInput, { target: { value: "TestPassword123!"} });
+    const errorMessage = screen.queryByText(/passwords do not match/i);
+    expect(errorMessage).not.toBeInTheDocument();
+  });
+
+});
