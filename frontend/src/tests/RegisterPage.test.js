ac319c3fbada7fce148de5c598004b01c680ca93
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-03-10 09:15:33 +0000

    finished correcting the register page test

--- frontend/src/tests/RegisterPage.test.js
+++ frontend/src/tests/RegisterPage.test.js
@@ -1,15 +1,16 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import RegisterPage from '../Components/RegisterPage/RegisterPage';
 import userEvent from "@testing-library/user-event";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe("RegisterPage", () => {
   test("renders RegisterPage component", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const header = await screen.findByText(/register an account/i);
     expect(header).toBeInTheDocument();
   });
   test("shows password when clicked on show icon", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const toggleEye = await screen.findByAltText("toggle password visibilty");
     userEvent.click(toggleEye);
     const passwordInput = await screen.findByLabelText(/password/i);
@@ -17,7 +18,7 @@ describe("RegisterPage", () => {
   });
 
   test("hides password when clicked on show icon", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const toggleEye = await screen.findByAltText("toggle password visibilty");
     userEvent.click(toggleEye);
     userEvent.click(toggleEye);
@@ -26,7 +27,7 @@ describe("RegisterPage", () => {
   });
 
   test("password validation error message is visible when entering a invalid password", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const passwordInput = await screen.findByLabelText(/password/i);
     fireEvent.blur(passwordInput, { target: { value: "password"} });
     const passwordError = await screen.findByText(/invalid password/i);
@@ -34,7 +35,7 @@ describe("RegisterPage", () => {
   });
 
   test("password validation error message is not visible when entering a valid password", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const passwordInput = await screen.findByLabelText(/password/i);
     fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
     await waitFor(() => {
@@ -44,7 +45,7 @@ describe("RegisterPage", () => {
   });
 
   test("email validation error message is visible when entering a invalid email", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const emailInput = await screen.findByLabelText(/email address/i);
     fireEvent.blur(emailInput, { target: { value: "notanemail"} });
     const emailError = await screen.findByText(/invalid email/i);
@@ -52,7 +53,7 @@ describe("RegisterPage", () => {
   });
 
   test("email validation error message is not visible when entering a valid email", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const emailInput = await screen.findByLabelText(/email address/i);
     fireEvent.blur(emailInput, { target: { value: "test@example.com"} });
     await waitFor(() => {
@@ -62,7 +63,7 @@ describe("RegisterPage", () => {
   });
 
   test("password mismatch error message is visible when entering two different passwords", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const passwordInput = await screen.findByLabelText(/password/i);
     const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
     fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
@@ -72,7 +73,7 @@ describe("RegisterPage", () => {
   });
 
   test("password mismatch error message is not visible when entering two same passwords", async () => {
-    render(<RegisterPage />);
+    render(<Route> <RegisterPage /> </Route>);
     const passwordInput = await screen.findByLabelText(/password/i);
     const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
     fireEvent.blur(passwordInput, { target: { value: "TestPassword123!"} });
