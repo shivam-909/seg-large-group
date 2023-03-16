56eac47189a38a6729933b2b98a6ada567f1d345
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-03-16 17:29:51 +0000

    made a few changes to the test

--- frontend/src/tests/RegisterPage.test.js
+++ frontend/src/tests/RegisterPage.test.js
@@ -1,26 +1,17 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import RegisterPage from '../Components/RegisterPage/RegisterPage';
-import userEvent from "@testing-library/user-event";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+const { render, screen, fireEvent, waitFor } = require('@testing-library/react');
+const RegisterPage = require('../Components/RegisterPage/RegisterPage');
+const userEvent = require("@testing-library/user-event");
+const { BrowserRouter, Routes, Route } = require('react-router-dom');
 
 describe("RegisterPage", () => {
   test("renders RegisterPage component", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
     const header = await screen.findByText(/register an account/i);
     expect(header).toBeInTheDocument();
   });
   test("shows password when clicked on show icon", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const toggleEye = await screen.findByAltText("toggle password visibilty");
     userEvent.click(toggleEye);
     const passwordInput = await screen.findByLabelText(/password/i);
@@ -28,12 +19,8 @@ describe("RegisterPage", () => {
   });
 
   test("hides password when clicked on show icon", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const toggleEye = await screen.findByAltText("toggle password visibilty");
     userEvent.click(toggleEye);
     userEvent.click(toggleEye);
@@ -42,12 +29,8 @@ describe("RegisterPage", () => {
   });
 
   test("password validation error message is visible when entering a invalid password", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const passwordInput = await screen.findByLabelText(/password/i);
     fireEvent.blur(passwordInput, { target: { value: "password"} });
     const passwordError = await screen.findByText(/invalid password/i);
@@ -55,12 +38,8 @@ describe("RegisterPage", () => {
   });
 
   test("password validation error message is not visible when entering a valid password", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const passwordInput = await screen.findByLabelText(/password/i);
     fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
     await waitFor(() => {
@@ -70,12 +49,8 @@ describe("RegisterPage", () => {
   });
 
   test("email validation error message is visible when entering a invalid email", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const emailInput = await screen.findByLabelText(/email address/i);
     fireEvent.blur(emailInput, { target: { value: "notanemail"} });
     const emailError = await screen.findByText(/invalid email/i);
@@ -83,12 +58,8 @@ describe("RegisterPage", () => {
   });
 
   test("email validation error message is not visible when entering a valid email", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const emailInput = await screen.findByLabelText(/email address/i);
     fireEvent.blur(emailInput, { target: { value: "test@example.com"} });
     await waitFor(() => {
@@ -98,12 +69,8 @@ describe("RegisterPage", () => {
   });
 
   test("password mismatch error message is visible when entering two different passwords", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const passwordInput = await screen.findByLabelText(/password/i);
     const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
     fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
@@ -113,12 +80,8 @@ describe("RegisterPage", () => {
   });
 
   test("password mismatch error message is not visible when entering two same passwords", async () => {
-    render(
-    <Router>
-      <Route path="/">
-        <RegisterPage />
-      </Route>
-    </Router>);
+    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+
     const passwordInput = await screen.findByLabelText(/password/i);
     const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
     fireEvent.blur(passwordInput, { target: { value: "TestPassword123!"} });
@@ -126,5 +89,4 @@ describe("RegisterPage", () => {
     const errorMessage = screen.queryByText(/passwords do not match/i);
     expect(errorMessage).not.toBeInTheDocument();
   });
-
 });
