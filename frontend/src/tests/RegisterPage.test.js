b77edd0b0655f3287545982df0e66122744d37aa
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-03-16 20:37:40 +0000

    changed the tests

--- frontend/src/tests/RegisterPage.test.js
+++ frontend/src/tests/RegisterPage.test.js
@@ -1,92 +1,56 @@
-const { render, screen, fireEvent, waitFor } = require('@testing-library/react');
-const RegisterPage = require('../Components/RegisterPage/RegisterPage');
-const userEvent = require("@testing-library/user-event");
-const { BrowserRouter, Routes, Route } = require('react-router-dom');
+import { render, screen, fireEvent } from '@testing-library/react';
+import RegisterPage from '../Components/RegisterPage/RegisterPage';
 
-describe("RegisterPage", () => {
-  test("renders RegisterPage component", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
-    const header = await screen.findByText(/register an account/i);
-    expect(header).toBeInTheDocument();
-  });
-  test("shows password when clicked on show icon", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+test('renders RegisterPage component without crashing', () => {
+  render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+});
 
-    const toggleEye = await screen.findByAltText("toggle password visibilty");
-    userEvent.click(toggleEye);
-    const passwordInput = await screen.findByLabelText(/password/i);
-    expect(passwordInput).toHaveAttribute("type", "text");
-  });
+test('toggleRole function toggles visibility of Company-Fields and Seeker-Fields sections', () => {
+  render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  const seekerFields = screen.getByTestId('Seeker-Fields');
+  const companyFields = screen.getByTestId('Company-Fields');
 
-  test("hides password when clicked on show icon", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  expect(seekerFields).toBeVisible();
+  expect(companyFields).not.toBeVisible();
 
-    const toggleEye = await screen.findByAltText("toggle password visibilty");
-    userEvent.click(toggleEye);
-    userEvent.click(toggleEye);
-    const passwordInput = await screen.findByLabelText(/password/i);
-    expect(passwordInput).toHaveAttribute("type", "password");
-  });
+  fireEvent.click(screen.getByText('Company'));
 
-  test("password validation error message is visible when entering a invalid password", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  expect(seekerFields).not.toBeVisible();
+  expect(companyFields).toBeVisible();
+});
 
-    const passwordInput = await screen.findByLabelText(/password/i);
-    fireEvent.blur(passwordInput, { target: { value: "password"} });
-    const passwordError = await screen.findByText(/invalid password/i);
-    expect(passwordError).toBeInTheDocument();
-  });
+test('togglePasswordVisibility function toggles password visibility', () => {
+  render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  const passwordField = screen.getByTestId('password');
+  const confirmPassField = screen.getByTestId('confirmPass');
+  const eyeIcon = screen.getByAltText('toggle password');
 
-  test("password validation error message is not visible when entering a valid password", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  expect(passwordField.type).toBe('password');
+  expect(confirmPassField.type).toBe('password');
 
-    const passwordInput = await screen.findByLabelText(/password/i);
-    fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
-    await waitFor(() => {
-      const passwordError = screen.queryByText(/invalid password/i);
-      expect(passwordError).not.toBeInTheDocument();
-    });
-  });
+  fireEvent.click(eyeIcon);
 
-  test("email validation error message is visible when entering a invalid email", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  expect(passwordField.type).toBe('text');
+  expect(confirmPassField.type).toBe('text');
 
-    const emailInput = await screen.findByLabelText(/email address/i);
-    fireEvent.blur(emailInput, { target: { value: "notanemail"} });
-    const emailError = await screen.findByText(/invalid email/i);
-    expect(emailError).toBeInTheDocument();
-  });
+  fireEvent.click(eyeIcon);
 
-  test("email validation error message is not visible when entering a valid email", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  expect(passwordField.type).toBe('password');
+  expect(confirmPassField.type).toBe('password');
+});
 
-    const emailInput = await screen.findByLabelText(/email address/i);
-    fireEvent.blur(emailInput, { target: { value: "test@example.com"} });
-    await waitFor(() => {
-      const emailError = screen.queryByText(/invalid email/i);
-      expect(emailError).not.toBeInTheDocument();
-    });
-  });
+test('checkPasswordMatch function validates passwords correctly', () => {
+  render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  const passwordField = screen.getByTestId('password');
+  const confirmPassField = screen.getByTestId('confirmPass');
+  const errorText = screen.getByText('Password does not match');
 
-  test("password mismatch error message is visible when entering two different passwords", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  fireEvent.change(passwordField, { target: { value: 'password' } });
+  fireEvent.change(confirmPassField, { target: { value: 'password' } });
 
-    const passwordInput = await screen.findByLabelText(/password/i);
-    const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
-    fireEvent.blur(passwordInput, { target: { value: "Password123!"} });
-    fireEvent.blur(confirmPasswordInput, { target: { value: "DifferentPassword123!"} });
-    const passwordMismatchError = await screen.findByText(/passwords do not match/i);
-    expect(passwordMismatchError).toBeInTheDocument();
-  });
+  expect(errorText).not.toBeVisible();
 
-  test("password mismatch error message is not visible when entering two same passwords", async () => {
-    render(<BrowserRouter><Routes><Route element={ <RegisterPage/> }/></Routes></BrowserRouter>);
+  fireEvent.change(confirmPassField, { target: { value: 'password1' } });
 
-    const passwordInput = await screen.findByLabelText(/password/i);
-    const confirmPasswordInput = await screen.findByLabelText(/confirm password/i);
-    fireEvent.blur(passwordInput, { target: { value: "TestPassword123!"} });
-    fireEvent.blur(confirmPasswordInput, { target: { value: "TestPassword123!"} });
-    const errorMessage = screen.queryByText(/passwords do not match/i);
-    expect(errorMessage).not.toBeInTheDocument();
-  });
+  expect(errorText).toBeVisible();
 });
