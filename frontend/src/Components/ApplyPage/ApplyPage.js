3828fa5ecbea3a4aab7ade02da48c1f7befddd84
Cem Ratip <cemratip@outlook.com>
2023-03-18 15:59:41 +0000

    API call implementation in progress

--- frontend/src/Components/ApplyPage/ApplyPage.js
+++ frontend/src/Components/ApplyPage/ApplyPage.js
@@ -1,26 +1,58 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Navbar from "../Navbar/Navbar";
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import {useParams} from "react-router-dom";
+import axios from "axios";
 
 export default function ApplyPage() {
     const {id} = useParams();
 
     // TODO: Show 'Error 404 Not Found' page if no id is provided
-    // TODO: Fetch job details using id
 
-    const screeningQuestions = [
-        ['Please list 2-3 dates and time ranges when you could do an interview.', false],
-        ['How many years of Zoho development experience do you have?', true],
-        ['Are you authorised to work in the United Kingdom?', true],
-        ['Will you be able to reliably commute or relocate to Romford, Greater London for this job?', true],
-    ];
-    const [CV, setCV] = useState(['Cem Ratip CV', 'https://seg-joblink.s3.eu-west-2.amazonaws.com/Cem+Ratip+CV.pdf']);
+    const [job, setJob] = useState({
+        title: '',
+        company: '',
+        location: '',
+        salary: '',
+        CV: [],
+        screeningQuestions: [],
+    });
+    // [
+    //     ['Please list 2-3 dates and time ranges when you could do an interview.', false],
+    //     ['How many years of Zoho development experience do you have?', true],
+    //     ['Are you authorised to work in the United Kingdom?', true],
+    //     ['Will you be able to reliably commute or relocate to Romford, Greater London for this job?', true],
+    // ]
+    // const [CV, setCV] = useState(['Cem Ratip CV', 'https://seg-joblink.s3.eu-west-2.amazonaws.com/Cem+Ratip+CV.pdf']);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/jobs/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                axios.get(`http://localhost:8000/api/user/${data.companyID}`)
+                    .then(response => response.json())
+                    .then (data => {
+                        setJob({...job, company: data.companyName});
+                    })
+                // TODO: Get user's CV
+
+                setJob({
+                    ...job,
+                    title: data.title,
+                    location: data.location,
+                    salary: data.salary,
+                    screeningQuestions: data.screeningQuestions,
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [job]);
 
     function validateApplication() {
-        if (!screeningQuestions.empty) {
+        if (!job.screeningQuestions.empty) {
             const answers = document.getElementsByTagName('textarea');
             const scrollElementIntoView = [];
             Array.from(answers).forEach(answer => {
@@ -36,7 +68,7 @@ export default function ApplyPage() {
                 scrollElementIntoView[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
             }
         }
-        if (!CV) {
+        if (!job.CV) {
             const uploadBtn = document.getElementById('upload');
             uploadBtn.scrollIntoView({ behavior: 'smooth', block: 'center' });
         }
@@ -44,8 +76,8 @@ export default function ApplyPage() {
 
     function uploadFile(e) {
         const file = e.target.files[0];
-        // const fileUrl =
-        setCV([file.name, '']);
+        const fileUrl = ''; // TODO: Upload file to S3 and get URL
+        setJob({...job, CV: [file.name, fileUrl]});
     }
 
     return (
@@ -63,10 +95,10 @@ export default function ApplyPage() {
                     </div>
                     <div className='bg-darker-grey h-[0.1px] my-5'></div>
                     <div className='pt-6'>
-                        {screeningQuestions.length > 0 &&
+                        {job.screeningQuestions.length > 0 &&
                             <div className='space-y-5 pb-12'>
                                 <p className='font-bold text-3xl flex flex-col'>Screening questions</p>
-                                {screeningQuestions.map(question => {
+                                {job.screeningQuestions.map(question => {
                                     return (
                                         <div className='flex flex-col justify-center space-y-5 py-2.5'>
                                             <p className='text-xl'>{question[0]}{question[1] && <span className='text-red'> *</span>}</p>
@@ -79,14 +111,14 @@ export default function ApplyPage() {
                     </div>
                     <div className='pb-12'>
                         <p className='font-bold text-3xl pb-10'>Select your CV</p>
-                        {CV ?
+                        {job.CV ?
                             <div>
                                 <div className='border rounded-md border-dark-theme-grey p-2 space-y-5 inline-block cursor-pointer'>
                                     <div className='flex justify-between items-center pt-2'>
-                                        <p>{CV[0]}</p>
+                                        <p>{job.CV[0]}</p>
                                         <i className="fas fa-check-circle fa-xl pr-1 text-dark-theme-grey"></i>
                                     </div>
-                                    <Document file={CV[1]}>
+                                    <Document file={job.CV[1]}>
                                         <Page pageNumber={1} className='border rounded-md overflow-ellipsis'/>
                                     </Document>
                                 </div>
