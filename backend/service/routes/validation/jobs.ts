4d7443fabdf381f4b33aadf636923ac27d38b217
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2023-03-20 00:25:53 +0000

    refactored code to reduce code duplication

--- backend/service/routes/validation/jobs.ts
+++ backend/service/routes/validation/jobs.ts
@@ -12,6 +12,85 @@ function isStringArray(arr:string[]): boolean{
     return true
 }
 
+function ValidateRequirements(requirements:any){
+    if(!Array.isArray(requirements)){
+        throw new Error(errors.ErrorRequirementsMustBeArray);
+    }
+    if(!isStringArray(requirements)){
+        throw new Error(errors.ErrorRequirementsMustBeStringArray);
+    }
+}
+
+function ValidateBenefits(benefits:any){
+    if(!Array.isArray(benefits)){
+        throw new Error(errors.ErrorBenefitsPostedMustBeArray);
+    }
+    if(!isStringArray(benefits)){
+        throw new Error(errors.ErrorBenefitsMustBeStringArray);
+    }
+}
+
+function ValidateCompensation(compensation:any){
+    if(!Array.isArray(compensation)){
+        throw new Error(errors.ErrorCompensationPostedMustBeArray);
+    }
+    if(compensation.length!=2){
+        throw new Error(errors.ErrorCompensationSize);
+    }
+    if(!isStringArray(compensation)){
+        throw new Error(errors.ErrorCompensationMustBeStringArray);
+    }
+}
+
+function ValidateDescription(description:any){
+    if(description.length < 800){
+        throw new Error(errors.ErrorJobDescriptionTooShort);
+    }
+    if(description.length > 2000){
+        throw new Error(errors.ErrorJobDescriptionTooLong);
+    }
+    if(typeof description !== 'string'){
+        throw new Error(errors.ErrorDescriptionMustBeString);
+    }
+}
+
+function ValidateType(type:any){
+    if(!Array.isArray(type)){
+        throw new Error(errors.ErrorTypeMustBeArray);
+    }
+    if(!isStringArray(type)){
+        throw new Error(errors.ErrorTypeMustBeStringArray);
+    }
+}
+
+function ValidateSchedule(schedule:any){
+    if(!Array.isArray(schedule)){
+        throw new Error(errors.ErrorScheduleMustBeArray);
+    }
+    if(!isStringArray(schedule)){
+        throw new Error(errors.ErrorScheduleMustBeStringArray);
+    }
+}
+
+function ValidateQualifications(qualifications:any){
+    if(!Array.isArray(qualifications)){
+        throw new Error(errors.ErrorQualificationsMustBeArray);
+    }
+    if(!isStringArray(qualifications)){
+        throw new Error(errors.ErrorQualificationsMustBeStringArray);
+    }
+}
+
+async function ValidateCompanyId(db:DB, companyID:any){
+    if(typeof companyID !== 'string'){
+        throw new Error(errors.ErrorCompanyIDMustBeString);
+    }
+    const company = await companiesdb.RetrieveCompanyByID(db, companyID);
+    if(!company){
+        throw new Error(errors.ErrorCompanyNotFound);
+    }
+}
+
 
 export async function AddListing(db: DB, body: any): Promise<void> {
     const { title, compensation, description, location, type, schedule, companyID, industry, coverLetterRequired, urgent, qualifications, datePosted, benefits, requirements, screeningQuestions} = body;
@@ -24,27 +103,11 @@ export async function AddListing(db: DB, body: any): Promise<void> {
     if(!compensation){
         throw new Error(errors.ErrorCompensationRequired);
     }
-    if(!Array.isArray(compensation)){
-        throw new Error(errors.ErrorCompensationPostedMustBeArray);
-    }
-    if(compensation.length!=2){
-        throw new Error(errors.ErrorCompensationSize);
-    }
-    if(!isStringArray(compensation)){
-        throw new Error(errors.ErrorCompensationMustBeStringArray);
-    }
+   ValidateCompensation(compensation);
     if(!description){
         throw new Error(errors.ErrorDescriptionRequired);
     }
-    if(description.length < 800){
-        throw new Error(errors.ErrorJobDescriptionTooShort);
-    }
-    if(description.length > 2000){
-        throw new Error(errors.ErrorJobDescriptionTooLong);
-    }
-    if(typeof description !== 'string'){
-        throw new Error(errors.ErrorDescriptionMustBeString);
-    }
+   ValidateDescription(description);
     if(!location){
         throw new Error(errors.ErrorLocationRequired);
     }
@@ -54,29 +117,16 @@ export async function AddListing(db: DB, body: any): Promise<void> {
     if(!type){
         throw new Error(errors.ErrorTypeRequired);
     }
-    if(!Array.isArray(type)){
-        throw new Error(errors.ErrorTypeMustBeArray);
-    }
-    if(!isStringArray(type)){
-        throw new Error(errors.ErrorTypeMustBeStringArray);
-    }
+    ValidateType(type);
 
     if(!schedule){
         throw new Error(errors.ErrorScheduleRequired);
     }
-    if(!Array.isArray(schedule)){
-        throw new Error(errors.ErrorScheduleMustBeArray);
-    }
-    if(!isStringArray(schedule)){
-        throw new Error(errors.ErrorScheduleMustBeStringArray);
-    }
-
+   ValidateSchedule(schedule);
     if(!companyID){
         throw new Error(errors.ErrorCompanyIDRequired);
     }
-    if(typeof companyID !== 'string'){
-        throw new Error(errors.ErrorCompanyIDMustBeString);
-    }
+
     if(!industry){
         throw new Error(errors.ErrorIndustryRequired);
     }
@@ -98,12 +148,7 @@ export async function AddListing(db: DB, body: any): Promise<void> {
     if(!qualifications){
         throw new Error(errors.ErrorQualificationsRequired);
     }
-    if(!Array.isArray(qualifications)){
-        throw new Error(errors.ErrorQualificationsMustBeArray);
-    }
-    if(!isStringArray(qualifications)){
-        throw new Error(errors.ErrorQualificationsMustBeStringArray);
-    }
+    ValidateQualifications(qualifications);
     if(!datePosted){
         throw new Error(errors.ErrorDatePostedRequired);
     }
@@ -112,26 +157,12 @@ export async function AddListing(db: DB, body: any): Promise<void> {
     }
 
     if(benefits){
-        if(!Array.isArray(benefits)){
-            throw new Error(errors.ErrorBenefitsPostedMustBeArray);
-        }
-        if(!isStringArray(benefits)){
-            throw new Error(errors.ErrorBenefitsMustBeStringArray);
-        }
+       ValidateBenefits(benefits);
     }
     if(requirements){
-        if(!Array.isArray(requirements)){
-            throw new Error(errors.ErrorRequirementsMustBeArray);
-        }
-        if(!isStringArray(requirements)){
-            throw new Error(errors.ErrorRequirementsMustBeStringArray);
-        }
+        ValidateRequirements(requirements);
     }
-
-    const company = await companiesdb.RetrieveCompanyByID(db, companyID);
-    if(!company){
-        throw new Error(errors.ErrorCompanyNotFound);
-    }
+    await ValidateCompanyId(db,companyID);
 
 }
 
@@ -146,43 +177,19 @@ export async function UpdateListing(db: DB, id:string, req: any): Promise<void> 
         throw new Error(errors.ErrorTitleMustBeString);
     }
     if(compensation){
-        if(!Array.isArray(compensation)){
-            throw new Error(errors.ErrorCompensationPostedMustBeArray);
-        }
-        if(compensation.length!=2){
-            throw new Error(errors.ErrorCompensationSize);
-        }
-        if(!isStringArray(compensation)){
-            throw new Error(errors.ErrorCompensationMustBeStringArray);
-        }
+        ValidateCompensation(compensation);
     }
     if(location && typeof location !== 'string'){
         throw new Error(errors.ErrorLocationMustBeString)
     }
     if(type){
-        if(!Array.isArray(type)){
-            throw new Error(errors.ErrorTypeMustBeArray);
-        }
-        if(!isStringArray(type)){
-            throw new Error(errors.ErrorTypeMustBeStringArray);
-        }
+        ValidateType(type);
     }
     if(schedule){
-        if(!Array.isArray(schedule)){
-            throw new Error(errors.ErrorTypeMustBeArray);
-        }
-        if(!isStringArray(schedule)){
-            throw new Error(errors.ErrorTypeMustBeStringArray);
-        }
+        ValidateSchedule(schedule);
     }
     if(companyID){
-        if(typeof companyID !== 'string'){
-            throw new Error(errors.ErrorCompanyIDMustBeString);
-        }
-        const company = await companiesdb.RetrieveCompanyByID(db, companyID);
-        if(!company){
-            throw new Error(errors.ErrorCompanyNotFound);
-        }
+        await ValidateCompanyId(db,companyID);
     }
     if(coverLetterRequired && typeof coverLetterRequired !== 'boolean'){
         throw new Error(errors.ErrorCoverLetterRequiredMustBeBoolean);
@@ -194,23 +201,10 @@ export async function UpdateListing(db: DB, id:string, req: any): Promise<void> 
         throw new Error(errors.ErrorIndustryMustBeString);
     }
     if (description) {
-        if(typeof description !== 'string'){
-            throw new Error(errors.ErrorDescriptionMustBeString);
-        }
-        if (description.length < 800) {
-            throw new Error(errors.ErrorJobDescriptionTooShort);
-        }
-        if (description.length > 2000) {
-            throw new Error(errors.ErrorJobDescriptionTooLong);
-        }
+       ValidateDescription(description);
     }
     if(qualifications){
-        if(!Array.isArray(qualifications)){
-            throw new Error(errors.ErrorQualificationsMustBeArray);
-        }
-        if(!isStringArray(qualifications)){
-            throw new Error(errors.ErrorQualificationsMustBeStringArray);
-        }
+        ValidateQualifications(qualifications);
     }
 
     if(datePosted && !(datePosted instanceof Date)){
@@ -218,20 +212,10 @@ export async function UpdateListing(db: DB, id:string, req: any): Promise<void> 
     }
 
     if(benefits){
-        if(!Array.isArray(benefits)){
-            throw new Error(errors.ErrorBenefitsPostedMustBeArray);
-        }
-        if(!isStringArray(benefits)){
-            throw new Error(errors.ErrorBenefitsMustBeStringArray);
-        }
+        ValidateBenefits(benefits);
     }
     if(requirements){
-        if(!Array.isArray(requirements)){
-            throw new Error(errors.ErrorRequirementsMustBeArray);
-        }
-        if(!isStringArray(requirements)){
-            throw new Error(errors.ErrorRequirementsMustBeStringArray);
-        }
+        ValidateRequirements(requirements);
     }
 
 
