a89737d8e8dd9c91255760543fdca173efb286a1
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2023-03-22 19:40:09 +0000

    added validation to searcher

--- service/routes/companies.ts
+++ service/routes/companies.ts
@@ -2,15 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { ErrorCompanyNotFound, Handler } from "../public";
 import DB from "../../db/db";
 import * as companiesdb from "../../db/companies";
+import {ValidateCompanyId} from "./validation/checks";
+import * as validate from "./validation/notifications";
 
 export function GetCompany(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
         const id = req.params.id;
-        const user = await companiesdb.RetrieveCompanyByID(db, id);
-        if (!user) {
-            next(ErrorCompanyNotFound);
-            return
+        try {
+            await ValidateCompanyId(db, id);
+        } catch (err) {
+            next((err as Error).message);
+            return;
         }
+        const user = await companiesdb.RetrieveCompanyByID(db, id);
         res.status(200).json(user);
     };
 }
