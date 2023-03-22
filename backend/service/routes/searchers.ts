10cf3bddf252e15afa7bb76aad0663340c84abf1
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2023-03-22 19:37:15 +0000

    added validation to searcher

--- backend/service/routes/searchers.ts
+++ backend/service/routes/searchers.ts
@@ -2,15 +2,13 @@ import { ErrorSearcherNotFound, Handler } from "../public";
 import { NextFunction, Request, Response } from "express";
 import DB from "../../db/db";
 import * as searchersdb from "../../db/searchers";
+import {ValidateSearcherId} from "./validation/checks";
 
 export function GetSearcher(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
         const id = req.params.id;
-
+        await ValidateSearcherId(db,id);
         const user = await searchersdb.RetrieveSearcherByID(db, id);
-        if (!user) {
-            next(ErrorSearcherNotFound);
-        }
         res.status(200).json(user);
     };
 }
