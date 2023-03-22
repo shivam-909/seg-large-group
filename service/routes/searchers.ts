fdde61680b056bac7c1a9eba66202cf4a9469afd
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2023-03-22 19:41:02 +0000

    minor changes

--- service/routes/searchers.ts
+++ service/routes/searchers.ts
@@ -1,4 +1,4 @@
-import { ErrorSearcherNotFound, Handler } from "../public";
+import {Handler } from "../public";
 import { NextFunction, Request, Response } from "express";
 import DB from "../../db/db";
 import * as searchersdb from "../../db/searchers";
@@ -7,7 +7,12 @@ import {ValidateSearcherId} from "./validation/checks";
 export function GetSearcher(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
         const id = req.params.id;
-        await ValidateSearcherId(db,id);
+        try {
+            await ValidateSearcherId(db, id);
+        } catch (err) {
+            next((err as Error).message);
+            return;
+        }
         const user = await searchersdb.RetrieveSearcherByID(db, id);
         res.status(200).json(user);
     };
