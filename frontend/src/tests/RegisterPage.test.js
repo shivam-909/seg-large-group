a0ed3daa7d2f1bb4c2f95da7baad174e6aa2e205
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-03-24 12:32:02 +0000

    making changes to the tests

--- frontend/src/tests/RegisterPage.test.js
+++ frontend/src/tests/RegisterPage.test.js
@@ -1,81 +1,160 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+// import React from 'react';
+// import { render, screen, fireEvent } from '@testing-library/react';
+// import RegisterPage from '../Components/RegisterPage/RegisterPage';
+// import {BrowserRouter, Route, Routes} from "react-router-dom";
+//
+//
+// test('renders RegisterPage component without crashing', () => {
+//   render(
+//     <BrowserRouter>
+//       <Routes>
+//         <Route element={<RegisterPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// });
+//
+// test('toggleRole function toggles visibility of Company-Fields and Seeker-Fields sections', () => {
+//   render(
+//     <BrowserRouter>
+//       <Routes>
+//         <Route element={<RegisterPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+//   const seekerFields = screen.getByRole('searcher', {hidden: true});
+//   const companyFields = screen.getByRole('company', {hidden: true});
+//
+//   expect(seekerFields).toHaveClass('hidden');
+//   expect(companyFields).not.toHaveClass('hidden');
+//   fireEvent.click(companyFields);
+//   expect(seekerFields).not.toHaveClass('hidden');
+//   expect(companyFields).toHaveClass('hidden');
+// });
+//
+// test('togglePasswordVisibility function toggles password visibility', () => {
+  // render(
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route element={<RegisterPage />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
+//   const passwordField = screen.getByTestId('password');
+//   const confirmPassField = screen.getByTestId('confirmPass');
+//   const eyeIcon = screen.getByAltText('toggle password');
+//
+//   expect(passwordField.type).toBe('password');
+//   expect(confirmPassField.type).toBe('password');
+//
+//   fireEvent.click(eyeIcon);
+//
+//   expect(passwordField.type).toBe('text');
+//   expect(confirmPassField.type).toBe('text');
+//
+//   fireEvent.click(eyeIcon);
+//
+//   expect(passwordField.type).toBe('password');
+//   expect(confirmPassField.type).toBe('password');
+// });
+//
+// test('checkPasswordMatch function validates passwords correctly', () => {
+//   render(
+//     <BrowserRouter>
+//       <Routes>
+//         <Route element={<RegisterPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+//   const passwordField = screen.getByTestId("password");
+//   expect(passwordField).toBeInTheDocument();
+//   const confirmPasswordField = screen.getByTestId('confirmPassword');
+//   const errorText = screen.getByText('Password does not match');
+//
+//   fireEvent.change(passwordField, { target: { value: 'password' } });
+//   fireEvent.change(confirmPasswordField, { target: { value: 'password' } });
+//
+//   expect(errorText).not.toBeVisible();
+//
+//   fireEvent.change(confirmPasswordField, { target: { value: 'password1' } });
+//
+//   expect(errorText).toBeVisible();
+// });
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import RegisterPage from '../Components/RegisterPage/RegisterPage';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+describe('RegisterPage', () => {
+  it('renders correctly', () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    );    expect(screen.getByText('Register an account')).toBeInTheDocument();
+  });
 
-test('renders RegisterPage component without crashing', () => {
-  render(
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-});
+  it('allows user to register with valid credentials', async () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+    // Fill in form fields
+    const roleButton = screen.getByRole('button', { name: 'Job Seeker' });
+    fireEvent.click(roleButton);
 
-test('toggleRole function toggles visibility of Company-Fields and Seeker-Fields sections', () => {
-  render(
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-  const seekerFields = screen.getByRole('searcher', hidden: true});
-  const companyFields = screen.getByRole('company', hidden: true});
+    const firstNameInput = screen.getByLabelText('First name');
+    fireEvent.change(firstNameInput, { target: { value: 'John' } });
 
-  expect(seekerFields).toHaveClass('hidden');
-  expect(companyFields).not.toHaveClass('hidden');
-  fireEvent.click(companyFields);
-  expect(seekerFields).not.toHaveClass('hidden');
-  expect(companyFields).toHaveClass('hidden');
-});
+    const lastNameInput = screen.getByLabelText('Last name');
+    fireEvent.change(lastNameInput, { target: { value: 'Doe' } });
 
-test('togglePasswordVisibility function toggles password visibility', () => {
-  render(
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-  const passwordField = screen.getByTestId('password');
-  const confirmPassField = screen.getByTestId('confirmPass');
-  const eyeIcon = screen.getByAltText('toggle password');
+    const emailInput = screen.getByLabelText('Email address');
+    fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
 
-  expect(passwordField.type).toBe('password');
-  expect(confirmPassField.type).toBe('password');
+    const passwordInput = screen.getByLabelText('Password');
+    fireEvent.change(passwordInput, { target: { value: 'P@ssw0rd' } });
 
-  fireEvent.click(eyeIcon);
+    const confirmPasswordInput = screen.getByLabelText('Confirm password');
+    fireEvent.change(confirmPasswordInput, { target: { value: 'P@ssw0rd' } });
 
-  expect(passwordField.type).toBe('text');
-  expect(confirmPassField.type).toBe('text');
+    // Submit form
+    const submitButton = screen.getByRole('button', { name: 'Sign up' });
+    fireEvent.click(submitButton);
 
-  fireEvent.click(eyeIcon);
+    // Wait for API call to complete
+    await waitFor(() => expect(screen.queryByText('Loading...')).not.toBeInTheDocument());
 
-  expect(passwordField.type).toBe('password');
-  expect(confirmPassField.type).toBe('password');
-});
+    // Verify that user is redirected to home page
+    expect(window.location.pathname).toBe('/');
+  });
 
-test('checkPasswordMatch function validates passwords correctly', () => {
-  render(
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-  const passwordField = screen.getByTestId("password");
-  expect(passwordField).toBeInTheDocument();
-  const confirmPasswordField = screen.getByTestId('confirmPassword');
-  const errorText = screen.getByText('Password does not match');
+  it('displays error message when API call fails', async () => {
+    // Mock axios.post to simulate API call failure
+    jest.spyOn(axios, 'post').mockImplementation(() => Promise.reject());
 
-  fireEvent.change(passwordField, { target: { value: 'password' } });
-  fireEvent.change(confirmPasswordField, { target: { value: 'password' } });
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+    // Fill in form fields
+    // ...
 
-  expect(errorText).not.toBeVisible();
+    // Submit form
+    // ...
 
-  fireEvent.change(confirmPasswordField, { target: { value: 'password1' } });
+    // Wait for API call to complete
+    await waitFor(() => expect(screen.queryByText('Loading...')).not.toBeInTheDocument());
 
-  expect(errorText).toBeVisible();
+    // Verify that error message is displayed
+    expect(screen.getByText('An error occurred. Please try again.')).toBeInTheDocument();
+  });
 });
