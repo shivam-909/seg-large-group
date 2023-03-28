06dc1cac8f02baf7b5e6eb94c276f40ab43f7bf8
Cem Ratip <cemratip@outlook.com>
2023-03-28 03:33:40 +0100

    bug fix

--- service/routes/search.ts
+++ service/routes/search.ts
@@ -3,16 +3,13 @@ import 'express-async-errors';
 import *  as validate from "../routes/validation/search";
 import DB from "../../db/db";
 import {getErrorMessage, Handler} from "../public";
-import * as errors from "../public";
 import { findJobListingsByQuery } from "../../search/search";
-import { isQuery } from "./validation/search";
 
 export function SearchListings(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
-
+        const query = req.body.term.toString().toLowerCase();
         await validate.isQuery(db,req.body);
-        const results = await findJobListingsByQuery(db, req.body);
+        const results = await findJobListingsByQuery(db, query);
         res.status(200).json({ results });
-
     };
 }
