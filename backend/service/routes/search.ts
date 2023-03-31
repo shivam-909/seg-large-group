db9c53b6dbefc5b72fc23c20b582431e65ccb7a6
Samiul I <99480280+Samiul687@users.noreply.github.com>
2023-04-01 00:25:01 +0100

    searcherID taken from headers for search and matchmaking

--- backend/service/routes/search.ts
+++ backend/service/routes/search.ts
@@ -4,21 +4,29 @@ import *  as validate from "../routes/validation/search";
 import DB from "../../db/db";
 import {Handler} from "../public";
 import {FindJobListingsByQuery, MatchmadeSearch} from "../../search/search";
+import {RetrieveFullUserByID} from "../../db/users";
+
 
 export function SearchListings(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
         const term = req.body.term.toString().toLowerCase();
         await validate.isQuery(db,req.body);
-        if (req.body.searcherID) {
-            const searcherID = req.body.searcherID;
-            const results = await MatchmadeSearch(db, term, searcherID);
-            res.status(200).json({ results });
-            return;
+
+        if (req.headers.auth_username) {
+            const userID = req.headers.auth_username as string;
+            const user = await RetrieveFullUserByID(db, userID);
+
+            const searcherID = user?.searcherID;
+            if (searcherID) {
+                const results = await MatchmadeSearch(db, term, searcherID);
+                return res.status(200).json({ results });
+            }
         }
-        else {
-            const results = await FindJobListingsByQuery(db, term);
-            res.status(200).json({ results });
-            return;
-        }
+
+        const results = await FindJobListingsByQuery(db, term);
+        return res.status(200).json({ results });
     };
 }
+
+
+
