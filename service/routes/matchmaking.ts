e2d95877b4d9da200b379fa16f5b0c3a17f37855
Samiul I <99480280+Samiul687@users.noreply.github.com>
2023-04-01 00:25:01 +0100

    searcherID taken from headers for search and matchmaking

--- service/routes/matchmaking.ts
+++ service/routes/matchmaking.ts
@@ -3,13 +3,17 @@ import { NextFunction, Request, Response } from "express";
 import DB from "../../db/db";
 import { Handler } from "../public";
 import {FindMatchesFromAllJobs } from "../../matchmaking/matchmaking";
+import {RetrieveFullUserByID} from "../../db/users";
 
 
 export function FindMatchingJobs(db: DB): Handler {
     return async (req: Request, res: Response, next: NextFunction) => {
-        const searcherId = req.params.id;
-        const jobListings = await FindMatchesFromAllJobs(db, searcherId);
+        const userID = req.headers.auth_username as string;
+        const user = await RetrieveFullUserByID(db, userID)
+        const searcherID = user!.searcherID!;
+        const jobListings = await FindMatchesFromAllJobs(db, searcherID);
 
         res.status(200).json(jobListings);
     };
 }
+
