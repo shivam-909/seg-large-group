7023ff4a9d77b392bd6fdc312a74ccc930c50893
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-04-01 11:39:35 +0100

    fixing the tests

--- frontend/src/tests/SearchPage.test.js
+++ frontend/src/tests/SearchPage.test.js
@@ -7,22 +7,22 @@ describe('SearchPage', () => {
   test('renders Search Page', () => {
     render(<BrowserRouter><Routes><Route element={ <SearchPage/> }/></Routes></BrowserRouter>);
   });
-  //it('displays job search results when valid inputs are entered',() => {
-    // render(<SearchPage />);
-    // const jobTitleInput = screen.getByPlaceholderText('Job title');
-    // const locationInput = screen.getByPlaceholderText('Location');
-    // const searchButton = screen.getByRole('button', {name: 'Search'});
-    // fireEvent.change(jobTitleInput, {target: {value:'Software Engineer'}});
-    // fireEvent.change(locationInput, {target: {value:'London, UK'}});
-    // fireEvent.click(searchButton);
+  it('displays job search results when valid inputs are entered',() => {
+    render(<BrowserRouter><Routes><Route element={ <SearchPage/> }/></Routes></BrowserRouter>);
+    const jobTitleInput = screen.getByPlaceholderText('Job title');
+    const locationInput = screen.getByPlaceholderText('Location');
+    const searchButton = screen.getByRole('button', {name: 'Search'});
+    fireEvent.change(jobTitleInput, {target: {value:'Software Engineer'}});
+    fireEvent.change(locationInput, {target: {value:'London, UK'}});
+    fireEvent.click(searchButton);
 
-  //});
+  });
 
-  //test('displays error messages when inputs are invalid', () => {
-    // render(<SearchPage />);
-    // const searchButton = screen.getByRole('button', {name: 'Search'});
-    // fireEvent.click(searchButton);
-    // expect(screen.getByText(/Please enter a job title/)).toBeInTheDocument();
-    // expect(screen.getByText(/Please enter a location/)).toBeInTheDocument();
-//  });
+  test('displays error messages when inputs are invalid', () => {
+    render(<BrowserRouter><Routes><Route element={ <SearchPage/> }/></Routes></BrowserRouter>);
+    const searchButton = screen.getByRole('button', {name: 'Search'});
+    fireEvent.click(searchButton);
+    expect(screen.getByText(/Please enter a job title/)).toBeInTheDocument();
+    expect(screen.getByText(/Please enter a location/)).toBeInTheDocument();
+  });
 });
