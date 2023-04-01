430f1b74fa32f82e635f07ee88670639bc2850ab
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-04-01 13:48:05 +0100

    changes

--- frontend/src/Components/ProfilePage/SkillCard.js
+++ frontend/src/Components/ProfilePage/SkillCard.js
@@ -6,7 +6,6 @@ export default function SkillCard(props) {
 
     function deleteSkill(){
         document.getElementById("Skill" + props.id).remove();
-
     }
     const validate = function(field, regex){
         let obj = document.getElementById(field);
@@ -18,26 +17,20 @@ export default function SkillCard(props) {
         }
     }
     return (
-        <div id={"Skill" + props.id} className={"grid grid-cols-2 gap-2 my-2"} data-testid='Skill'>
-           <label htmlFor={"skillInput" + props.id}>Skill</label>
+        <div id={"Skill" + props.id} className={"grid grid-cols-2 gap-2 my-2"}>
             <input id={"skillInput" + props.id} onChange={() => {validate("skillInput"+props.id,/^([^,]*)$/)}} className={"key"} placeholder={"Skill"} editable={"true"} defaultValue={props.skill}/>
             <div>
             <div className={"value w-[70%] float-left"}>
-                <label htmlFor={"skillDuration"}>Duration</label>
                 <input id={"skillDuration"} className={"duration"} placeholder={"Duration"} type={"number"} min={"0"} editable={"true"} defaultValue={props.duration}/>
-                <label htmlFor={"skillInterval"}>Interval</label>
                 <select id={"skillInterval"} defaultValue={props.interval} className={"w-[60%]"} editable={"true"}>
                     <option value={"weeks"}>Week/s</option>
                     <option value={"months"}>Month/s</option>
                     <option value={"years"}>Year/s</option>
                 </select>
             </div>
-            <button className={"delete w-[24%]"} onClick={deleteSkill} editable={"true"}><i className="fa-solid fa-trash"></i>Delete</button>
+            <button className={"delete w-[24%]"} onClick={deleteSkill} editable={"true"}><i className="fa-solid fa-trash"></i></button>
             </div>
-
-            {!valid && <div className={"text-red"} data-testid='Error'>
-            Please remove all commas
-            </div>}
+            {!valid && <div className={"text-red"}>Please remove all commas</div>}
         </div>
     );
 }
