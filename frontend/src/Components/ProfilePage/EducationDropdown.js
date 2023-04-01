b21eb031a7e1068a70d9c35fcb3020cd4dcff74f
Sifa Shaikh <k21062358@kcl.ac.uk>
2023-04-01 13:44:04 +0100

    changing the code

--- frontend/src/Components/ProfilePage/EducationDropdown.js
+++ frontend/src/Components/ProfilePage/EducationDropdown.js
@@ -1,6 +1,5 @@
 import './Skills.css';
-import React from 'react';
-import {useState} from "react";
+import React, {useState} from "react";
 
 export default function EducationDropdown(props) {
     const [valid, setValid] = useState(true);
@@ -8,11 +7,11 @@ export default function EducationDropdown(props) {
         document.getElementById("Education" + props.id).remove();
     }
     const validate = function(){
-        const regex = /^([^,]*)$/;
+        const regex = /^([^,]*)$/
         let grade = document.getElementById("grade"+props.id).value;
         let subject = document.getElementById("subject"+props.id).value;
         if (regex.test(grade) && regex.test(subject)){
-            setValid(true);
+            setValid(true)
         }
         else{
             setValid(false);
