b108bd01f511b7415449b7e224b9f7a8c7aa9a0b
Tahseen Humaira <humairatahseen4@gmail.com>
2023-04-03 00:05:39 +0100

    Added test suite

--- frontend/src/tests/TextInputBox.test.js
+++ frontend/src/tests/TextInputBox.test.js
@@ -3,14 +3,6 @@ import { render} from '@testing-library/react';
 import TextInputBox from '../Components/LoginPage/TextInputBox';
 
 describe ('TextInputBox component', () => {
-    const props ={
-        id: 'email',
-        type: 'email',
-        onChange:'',
-        onBlur: '',
-        placeholder:''
-    };
-    
         it ('should render component on the screen', () => {
             render (<TextInputBox/>);
             expect(true).toBeTruthy();
