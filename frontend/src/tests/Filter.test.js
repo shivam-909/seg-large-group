7217aa8d3369c31d55f3ce1f7c131d860fdb2c1b
Oneeb Aamer <oneeb47@gmail.com>
2023-04-03 07:11:23 +0100

    Fix PDF view for Application

--- frontend/src/tests/Filter.test.js
+++ frontend/src/tests/Filter.test.js
@@ -6,7 +6,6 @@ jest.mock('react-router-dom', () => ({
 import {render, screen, waitFor} from "@testing-library/react";
 import { initialize } from "@googlemaps/jest-mocks";
 import userEvent from '@testing-library/user-event';
-import SearchBar from "../Components/SearchPage/SearchBar";
 import Filters from "../Components/SearchPage/Filters";
 import { setupJestCanvasMock } from 'jest-canvas-mock';
 
@@ -74,39 +73,4 @@ describe('Filter', () => {
         userEvent.click(screen.getByText('Clear'));
         waitFor(() => {expect(screen.getByText('Show filters')).toBeVisible()});
     });
-
-    test('snapshot test for setting filter', () => {
-        const props = {
-            jobs: [],
-        };
-        const {container} = render(
-            <Filters {...props}/>
-        );
-        waitFor(() => {expect(screen.getByText('Show filters')).toBeVisible()});
-        userEvent.click(screen.getByText('Show filters'));
-        waitFor(() => {expect(screen.getByText('Hide filters')).toBeVisible()});
-    //     userEvent.selectOptions(screen.getByLabelText('Date Posted'),['3']);
-    //     userEvent.selectOptions(screen.getByLabelText('Distance'),['10']);
-    //     userEvent.selectOptions(screen.getByLabelText('Salary'),['75000']);
-    //     userEvent.selectOptions(screen.getByLabelText('Job type'),['Permanent']);
-    //     userEvent.selectOptions(screen.getByLabelText('Workplace'),['Remote']);
-    //     userEvent.selectOptions(screen.getByLabelText('Industry'),['Engineering']);
-    //     userEvent.selectOptions(screen.getByLabelText('Location'),['Toronto']);
-    //     userEvent.selectOptions(screen.getByRole("button", {name: 'Company'}),['Google']);
-       expect(container).toMatchSnapshot();
-     });
-
-//     userEvent.selectOptions(screen.getByLabelText('Date'),['3']);
-//     userEvent.selectOptions(screen.getByLabelText('Distance'),['10']);
-//     userEvent.selectOptions(screen.getByLabelText('Salary'),['75000']);
-//     userEvent.selectOptions(screen.getByLabelText('Job Type'),['Permanent']);
-//     userEvent.selectOptions(screen.getByLabelText('Situation'),['Remote']);
-//
-  //   expect(screen.getByLabelText('Date')).toHaveValue('3');
-  //   expect(screen.getByLabelText('Distance')).toHaveValue('10');
-  //   expect(screen.getByLabelText('Salary')).toHaveValue('75000');
-  //   expect(screen.getByLabelText('Job Type')).toHaveValue('Permanent');
-  //   expect(screen.getByLabelText('Situation')).toHaveValue('Remote');
-  //
-  // });
 });
